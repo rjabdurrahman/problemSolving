db0a1bb39f0b1b64987dbae4418dff3e9405963f
rjabdurrahman <md.rahman0296@gmail.com>
2019-10-14 00:59:42 +0600

    Reduce: array-left-rotation memory complexity

--- DataStructure/array-left-rotation.cpp
+++ DataStructure/array-left-rotation.cpp
@@ -4,20 +4,22 @@ using namespace std;
 
 int main()
 {
-    int n, d, i;
+    int n, d, i, p;
     cin >> n;
     cin >> d;
-    int part1[d], part2[n - d];
+    int part1[d];
     for(i = 0; i < n; i++)
     {
         if(i < d)
         {
             cin >> part1[i];
         }
-        else cin >> part2[i - d];
+        else
+        {
+            cin >> p;
+            cout << p << " ";;
+        }
     }
-
-    for(i = 0; i < n - d; i++) cout << part2[i] << " ";
     for(i = 0; i < d; i++) cout << part1[i] << " ";
     cout << endl;
 
